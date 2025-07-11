0df9561bf5199b3a4b39a4a79e697d99ade5e0c2
yelantsevv <yelantsevv@gmail.com>
2025-07-11 14:15:40 +0300

    fix: mirror of original SWAPI on original SSL certificate expiredfeat

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,6 @@
-export const URL = 'https://swapi.dev/api/people/';
+// export const URL = 'https://swapi.dev/api/people/';
+// зеркало оригинального SWAPI на оригинальном SSL-сертификате истекло
+export const URL = 'https://swapi.py4e.com/api/people';
 
 export const getData = async <T>(page: string): Promise<T> => {
   const response = await fetch(page);
