1e05cf57323e03963abdde13d807cdf73636cf0a
yelantsevv <yelantsevv@gmail.com>
2025-07-17 14:30:22 +0300

    feat: add basic tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,28 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/Class-components/',
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/__tests__/setup.ts'],
+    coverage: {
+      thresholds: {
+        // statements: 80,
+        // branches: 50,
+        // functions: 50,
+        // lines: 50,
+      },
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      exclude: [
+        '**/main.tsx',
+        '**/types/**',
+        '**/__tests__/**',
+        '**/vite-env.d.ts',
+      ],
+    },
+  },
 });
